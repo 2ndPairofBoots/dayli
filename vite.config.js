08bec948a9e3b59bd5bdbeaab880a6b124f52e30
Alexander Li <alexandermli2010@gmail.com>
2026-03-17 08:55:28 -0400

    add README and update app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/dayli/'
+  base: '/dayli/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react-dom') || id.includes('react/')) return 'react';
+          if (id.includes('recharts')) return 'recharts';
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600,
+  }
 })
